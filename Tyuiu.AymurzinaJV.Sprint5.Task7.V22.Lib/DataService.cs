335c567ad4e4bba67fb012c98d4e9a6e97a8d142
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-11-29 13:16:49 +0500

    Task7 added

---
+++ Tyuiu.AymurzinaJV.Sprint5.Task7.V22.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AymurzinaJV.Sprint5.Task7.V22.Lib
+{
+    public class DataService : ISprint5Task7V22
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V22.txt";
+
+            FileInfo fileinfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileinfo.Exists;
+            if (fileExists) File.Delete(pathSaveFile);
+
+            string str = "";
+
+            using(StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if ((line[i] == '!') || (line[i] == '?') || (line[i] == '-') || (line[i] == '.') || (line[i] == ','))
+                        {
+                            str += '#';
+                        }
+                        else
+                        {
+                            str += line[i];
+                        }
+                    }
+                    File.AppendAllText(pathSaveFile, str + "\n");
+                    str = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint5.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.AymurzinaJV.Sprint5.Task7.V22.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint5.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\plans\source\repos\Tyuiu.AymurzinaJV.Sprint5\Tyuiu.AymurzinaJV.Sprint5.Task7.V22\bin\Debug\net8.0\OutPutDataFileTask7V22.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
